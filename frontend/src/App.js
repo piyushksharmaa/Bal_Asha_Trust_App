88b70ebdf21f42d661847ad49b447a59c39453c2
Piyush Kumar Sharma <pks05072001@gmail.com>
2023-06-03 00:16:26 +0530

    Adding styles to app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
+import "./styles/Header.scss";
 function App() {
   return (
     <div className="App">
