c96f8f80aacb272c6d37ff682694a38caf660e4d
Piyush Kumar Sharma <pks05072001@gmail.com>
2023-06-03 00:11:56 +0530

    Front end Application for Bal-Asha-Trust

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
