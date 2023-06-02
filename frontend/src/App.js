beca633d624b9c584524186a64948775a6e1784d
Piyush Kumar Sharma <pks05072001@gmail.com>
2023-06-03 00:56:44 +0530

    Changed Nav-bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
 import "./styles/Header.scss";
+import "./styles/App.scss";
 function App() {
   return (
     <div className="App">
